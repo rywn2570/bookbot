073cdc56385b0c0520993d7151cdf0ced8109c80
rywn2570 <rywn2570@gmail.com>
2026-09-20 22:40:03 +1000

    Add source comments to main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from stats import count_words, count_characters, chars_dict_to_sorted_list
 import sys
 
 if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
+    print("Usage: python3 main.py <path_to_book>") #From boot.dev!
     sys.exit(1)
 else:
     book_path = sys.argv[1]
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
-        file_contents = f.read()
+        file_contents = f.read() #From bootdev
     return file_contents
 
 def main():
@@ -20,7 +20,7 @@ def main():
     print_report(book_path, num_words, listOfSorted)
 
 def print_report(book_path, num_words, listOfSorted):
-
+#Also from bootdev!
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
